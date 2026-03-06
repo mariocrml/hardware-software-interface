1d62b7650cb7ce0382d12144cbb4f9e006e9800d
Cioromila <mario.cioromila@stud.acs.upb.ro>
2026-03-06 20:13:18 +0200

    Added solution

--- labs/lab-01/tasks/rotations/support/rotate.c
+++ labs/lab-01/tasks/rotations/support/rotate.c
@@ -6,14 +6,20 @@
 
 void rotate_left(unsigned int *number, int bits)
 {
-	/* TODO */
-	(void) number;
-	(void) bits;
+	if(bits == 0) {
+		return;
+	}
+
+	unsigned int valoare = *number;
+	*number = (valoare << bits) | (valoare >> (32-bits));
 }
 
 void rotate_right(unsigned int *number, int bits)
 {
-	/* TODO */
-	(void) number;
-	(void) bits;
+	if(bits == 0) {
+		return;
+	}
+
+	unsigned int valoare = *number;
+	*number = (valoare >> bits) | (valoare << (32-bits));
 }
