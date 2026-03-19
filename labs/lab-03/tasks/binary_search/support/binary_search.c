a67773556cb031a446d79f75359a9c156d98c9ae
Cioromila <mario.cioromila@stud.acs.upb.ro>
2026-03-19 13:23:01 +0200

    lab03: Binary-Search, Added solution
    Signed-off-by: Cioromila <mario.cioromila@stud.acs.upb.ro>

--- labs/lab-03/tasks/binary_search/support/binary_search.c
+++ labs/lab-03/tasks/binary_search/support/binary_search.c
@@ -9,9 +9,38 @@ int binary_search(int *v, int len, int dest)
 	int start = 0;
 	int end = len - 1;
 	int middle;
+	int rezultat = -1;
 
-	/**
-	 * TODO: Implement binary search
-	 */
-	return -1;
+	Main:
+    if (start > end) {
+        goto Return;
+    }
+
+    middle = (start + end) / 2;
+
+    if (*(v + middle) == dest) {
+        goto Gasit;
+    }
+    if (*(v + middle) > dest) {
+        goto Little;
+    }
+    if (*(v + middle) < dest) {
+        goto Big;
+    }
+
+    Little:
+    end = middle - 1;
+    goto Main;
+
+    Big:
+    start = middle + 1;
+    goto Main;
+
+    Gasit:
+    rezultat = middle;
+    goto Return;
+
+    Return:
+    return rezultat;
 }
+
