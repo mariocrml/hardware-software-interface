38445bb06d2ab519a7e7fb860645d25d353d51d4
Cioromila <mario.cioromila@stud.acs.upb.ro>
2026-03-20 20:15:26 +0200

    lab02: pixels.c: Added solution
    Signed-off-by: Cioromila <mario.cioromila@stud.acs.upb.ro>

--- labs/lab-02/tasks/pixels/support/pixels.c
+++ labs/lab-02/tasks/pixels/support/pixels.c
@@ -8,13 +8,25 @@
 
 #include "pixel.h"
 #include "pixels.h"
+#define GET_PIXEL(a, i ,j) (*(*((a) + (i)) + (j)))
+
 
 void reverse_pic(struct picture *pic)
 {
-	(void) pic;
+	for (int i = 0; i < pic->height / 2; i++) {
+		struct Pixel *temp = pic->pix_array[i];
+        pic->pix_array[i] = pic->pix_array[pic->height - 1 - i];
+        pic->pix_array[pic->height - 1 - i] = temp;
+	}
 }
 
 void color_to_gray(struct picture *pic)
 {
-	(void) pic;
+	for(int i = 0; i < pic->height; i++) {
+		for(int j = 0; j < pic->width; j++) {
+			GET_PIXEL(pic->pix_array, i, j).R *= 0.3;
+			GET_PIXEL(pic->pix_array, i, j).G *= 0.59;
+			GET_PIXEL(pic->pix_array, i, j).B *= 0.11;
+		}
+	}
 }
