8c1c0d928628149aee63975e6a75ae2bae859c97
Cioromila <mario.cioromila@stud.acs.upb.ro>
2026-04-17 23:51:07 +0300

    lab04: Added solution for fibonacci.asm
    Signed-off-by: Cioromila <mario.cioromila@stud.acs.upb.ro>

--- labs/lab-04/tasks/fibonacci/support/fibonacci.asm
+++ labs/lab-04/tasks/fibonacci/support/fibonacci.asm
@@ -8,7 +8,8 @@ section .data
 section .text
     global main
     extern printf
-
+# 0 1 1 2 3 5 8 13 21 34 55 89 144
+# 0 1 2 3 4 5 6 7  8. 9. 10.11 12
 main:
     push rbp
     mov rbp, rsp
@@ -17,7 +18,23 @@ main:
     PRINTF64 `%d\n\x0`, rcx  ; DO NOT REMOVE/MODIFY THIS LINE
 
     ; TODO: calculate the N-th fibonacci number (f(0) = 0, f(1) = 1)
+    mov rax, 0
+    mov rbx, 1
+    mov r15, rcx
 
+    iteration:
+        mov r9, rax
+        add r9, rbx
+        mov rax, rbx
+        mov rbx, r9
+        dec r15
+        cmp r15, 0
+        je exit
+        jg iteration
+
+
+    exit:
+    PRINTF64 `%d\n\x0`, rax
     xor rax, rax
 
     leave
