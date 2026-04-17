f99e32922a79467296579de203dc3066a2c5ccad
Cioromila <mario.cioromila@stud.acs.upb.ro>
2026-04-17 20:50:31 +0300

    lab04: Added solution for sets.asm
    Signed-off-by: Cioromila <mario.cioromila@stud.acs.upb.ro>

--- labs/lab-04/tasks/sets/support/sets.asm
+++ labs/lab-04/tasks/sets/support/sets.asm
@@ -21,22 +21,38 @@ main:
     PRINTF64 `%u\n\x0`, rbx ; print the second set
 
     ; TODO1: reunion of the two sets
+    mov r12, rax
+    or r12, rbx
+    PRINTF64 `%u\n\x0`, r12
 
 
     ; TODO2: adding an element to a set
+    or rax, 768
+    PRINTF64 `%u\n\x0`, rax
 
 
     ; TODO3: intersection of the two sets
+    mov r12, rax
+    and r12, rbx
+    PRINTF64 `%u\n\x0`, r12
 
 
     ; TODO4: the complement of a set
-
+    mov r12, rax
+    not r12
+    PRINTF64 `%u\n\x0`, r12
 
     ; TODO5: removal of an element from a set
-
+    mov r13, 1
+    not r13
+    and rax, r13
+    PRINTF64 `%u\n\x0`, rax
 
     ; TODO6: difference of two sets
-
+    mov r12, rbx
+    not r12
+    and r12, rax
+    PRINTF64 `%u\n\x0`, r12
     xor rax, rax
 
     leave
