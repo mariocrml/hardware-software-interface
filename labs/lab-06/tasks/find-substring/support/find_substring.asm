8864c81ec610ca1c7b15f6c083a7558e1ca4f1e8
Cioromila <mario.cioromila@stud.acs.upb.ro>
2026-05-27 14:02:03 +0300

    lab06: Added solution for find_substring.asm
    Signed-off-by: Cioromila <mario.cioromila@stud.acs.upb.ro>

--- labs/lab-06/tasks/find-substring/support/find_substring.asm
+++ labs/lab-06/tasks/find-substring/support/find_substring.asm
@@ -16,6 +16,44 @@ main:
     mov rbp, rsp
 
     ; TODO: Print the start indices for all occurrences of the substring in source_text
+    mov r8, source_text
+    mov r9, substring
 
+source_loop:
+    mov rax, r8
+    mov rdx, r9
+
+substr_loop:
+    cmp byte [rdx], 0
+    je found_substr
+    mov bl, byte [rax]
+    cmp byte [rdx], bl
+    jne exit_substr_loop
+    inc rax
+    inc rdx
+    jmp substr_loop
+
+found_substr:
+    push rax
+    push rdx
+    push r8
+    push r9
+    mov rsi, r8
+    sub rsi, source_text
+    mov rdi, print_format
+    xor al, al
+    call printf
+    pop r9
+    pop r8
+    pop rdx
+    pop rax
+
+exit_substr_loop:
+    inc r8
+    cmp byte [r8], 0
+    je exit
+    jmp source_loop
+
+exit:
     leave
     ret
