0a9dda3ee76142c8a60dbc6c5b72d2eebdbc7d54
Cioromila <mario.cioromila@stud.acs.upb.ro>
2026-05-27 14:35:12 +0300

    lab08: Added solution for to_upper.asm
    Signed-off-by: Cioromila <mario.cioromila@stud.acs.upb.ro>

--- labs/lab-08/tasks/to-upper/support/to_upper.asm
+++ labs/lab-08/tasks/to-upper/support/to_upper.asm
@@ -6,11 +6,26 @@ to_upper:
     push rbp
     mov rbp, rsp
 
+    sub rsp, 8              ; leave room in order to align the stack
+    push rbx                ; preserve rbx as required by the System V AMD64 ABI
+
     ; TODO: save the used registers and align the stack, if needed
 
+    mov rax, rdi
     ; TODO: convert the string to uppercase
+check_one_byte:
+    mov bl, [rax]
+    test bl, bl
+    je out
+    sub bl, 0x20
+    mov [rax], bl
+    inc rax
+    jmp check_one_byte
 
+out:
     ; TODO: restore the used registers and the stack pointer, if altered
+    pop rbx
+    add rsp, 8
 
     leave
     ret
