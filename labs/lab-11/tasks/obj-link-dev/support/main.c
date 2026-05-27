dd7405cf007804a3e5599a1596125e053c5b4c97
Cioromila <mario.cioromila@stud.acs.upb.ro>
2026-05-27 16:55:48 +0300

    lab11: Added solution for obj-link-dev
    Signed-off-by: Cioromila <mario.cioromila@stud.acs.upb.ro>

--- labs/lab-11/tasks/obj-link-dev/support/main.c
+++ labs/lab-11/tasks/obj-link-dev/support/main.c
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
+extern int set_price(int price);
+extern void print_price(void);
+
+extern int qty;
+extern void print_quantity(void);
+
 int main(void)
 {
-	/*
-	 * TODO: Make it so you print:
-	 *    price is 21
-	 *    quantity is 42
-	 * without directly calling a printing function.
-	 */
+	set_price(21);
+	print_price();
+
+	qty = 42;
+	print_quantity();
 
 	return 0;
 }
